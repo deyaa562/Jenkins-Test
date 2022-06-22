894964b28be2fef718eb4d6cef9956751d5bfda0
deyaa562 <56220791+deyaa562@users.noreply.github.com>
2022-06-22 23:18:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import sys
 
 logger = Logger('logger')
 test = 'Test'
+test = sys.argv[1]
 
 if __name__ == '__main__':
-    logger.info(test)
-    logger.info(f'logger: {sys.argv[1]}')
+    logger.debug(test)
+    logger.debug(f'logger: {sys.argv[1]}')
+    logger.debug(f'logger: {(sys.argv[2]) + 1}')
+    logger.debug(f'logger: {(sys.argv[3]) + 4}')
+    print(sys.argv[1])
